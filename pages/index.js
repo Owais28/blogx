b10755d781449b972edbde77ddcfd6ae7dccea4a
Owais Athar <owaisathar2882@gmail.com>
2022-07-14 00:10:38 +0530

    Initial

--- pages/index.js
+++ pages/index.js
@@ -53,16 +53,21 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
+        {/* <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        > */}
+        <span 
+        className={
+          styles.font_footer
+        }>
+        BlogX
+        </span>
+          {/* <span className={styles.logo}>
+            {/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
+          {/* </span> */}
+        {/* </a> */}
       </footer>
     </div>
   )
